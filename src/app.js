750cb20ba90c95eb177185d6b8d68119f192210f
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-01-20 14:21:55 +0100

    Controle champs formulaire du haut

---
+++ src/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector("#content-flight form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let from = e.target.from.value;
+    let to = e.target.to.value;
+    if (isEmpty(from)) return alert("Le champs From n'est pas renseigné");
+    if (isEmpty(to)) return "Le champs From n'est pas renseigné";
+
+    console.log(`From: ${from} to: ${to}`);
+  });
+});
+
+/**
+ * Vérifie si la valeur est vide
+ * @param {String} value
+ * @returns
+ */
+const isEmpty = (value) => {
+  if (value === "" || value === null || value === undefined) {
+    return true;
+  }
+  return false;
+};
