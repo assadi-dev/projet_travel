f07860119610707cd1d58060bbdd924bd22b0dff
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-01-20 15:14:52 +0100

    Correction

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let to = e.target.to.value;
     if (isEmpty(from)) return alert("Le champs From n'est pas renseigné");
     if (isEmpty(to)) return alert("Le champs to n'est pas renseigné");
-    console.log(`From: ${from} to: ${to}`);
+    alert(`Success! : From: ${from} to: ${to}`);
   });
 });
 
